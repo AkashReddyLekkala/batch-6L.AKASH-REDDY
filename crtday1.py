3719f57f0c44a9c4ae43b190a34845bc29baea78
AkashReddyLekkala <162981841+AkashReddyLekkala@users.noreply.github.com>
2024-03-11 16:06:13 +0530

    Add files via upload

---
+++ crtday1.py
@@ -0,0 +1,71 @@
+'''
+print("hello world")
+
+print("hello world")
+print("hello world")
+print("hello world")
+
+print("hello world")
+
+'''
+#import this
+
+'''
+import antigravity
+'''
+
+'''
+Bargoined characters
+   Controls and data structures ---> ABC language
+   Strings and its formats ---> from C
+   Regular expressions ---> Pearl
+   Exception handling ---> Module 3
+   Magic methods from ---> fortron
+  '''
+
+'''
+Flavours of python
+ *) Cpython -- Written in C, Most common Implementation of python
+ *) Jython -- Java implementation of python.
+ *) 
+'''
+#print(object,sep,end,file,flush)
+#print has 5 paarmeters
+
+
+#object --->the vaiue to be printed is object
+#sep--->seperator
+#eg
+'''
+a=8
+b=9
+c=89
+print(a,b,c,  sep='$')
+'''
+#end -----> which is used to add the string at the end of the value
+## it will not keep the command to stay in same line
+#eg
+'''
+print(str(a),b,end='%%')
+print(str(c))
+'''
+#!---> variables
+# ?varible i  name which is used to  identify  the value of variable
+# ?which is stored in a specific memory location
+
+# ! Rules to dedfine variables
+# 1.) Variable have to start with only alphabet, underscores
+# 2.) The char following the 1st char can be alphabets, numbers, underscorse
+# 3.) No spaces are allowed
+# 4.) No special characters like !@#$%^&*()+
+# 5.) case sensitive
+# 6.) Keywords are not allowed as variables
+
+# ! ways to define variables
+'''
+a = 6
+print(a)
+print(type(a))
+'''
+
+
